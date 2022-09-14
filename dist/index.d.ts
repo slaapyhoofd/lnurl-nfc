744e7d44d8e2a330f6be5027e3b2a7885206fbe3
Jesse de Wit <witdejesse@hotmail.com>
2022-09-14 23:53:11 +0200

    new build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,23 @@
+/// <reference types="w3c-web-nfc" />
 export interface IPaymentResult {
     success: boolean;
     message: string;
 }
+export declare enum ErrorReason {
+    unavailable = 0,
+    aborted = 1,
+    scanInProgress = 2,
+    permissionDenied = 3,
+    readingError = 4,
+    noLnurlFound = 5
+}
 export declare class NFCReader {
     private readonly available;
     private readonly ndefReader;
-    private listening;
     constructor();
-    listen(callback: (lnurl: string) => void): Promise<void>;
+    listen(signal?: AbortSignal): Promise<string>;
+    _onReadingError(reject: (reason: ErrorReason) => void): void;
+    _onReading(event: NDEFReadingEvent, resolve: (value: string) => void, reject: (reason: ErrorReason) => void): void;
 }
-export declare function lnurlDecode(lnurl: string): string;
-export declare function bin2String(array: number[]): string;
 export declare function handleLNURL(lnurl: string, invoice: string, proxy: string): Promise<IPaymentResult>;
 export declare function handlePayment(callback: string, k1: string, invoice: string, proxy: string): Promise<IPaymentResult>;
--- dist/index.js
+++ dist/index.js
@@ -9,11 +9,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handlePayment = exports.handleLNURL = exports.bin2String = exports.lnurlDecode = exports.NFCReader = void 0;
+exports.handlePayment = exports.handleLNURL = exports.NFCReader = exports.ErrorReason = void 0;
 const bech32_1 = require("bech32");
+var ErrorReason;
+(function (ErrorReason) {
+    ErrorReason[ErrorReason["unavailable"] = 0] = "unavailable";
+    ErrorReason[ErrorReason["aborted"] = 1] = "aborted";
+    ErrorReason[ErrorReason["scanInProgress"] = 2] = "scanInProgress";
+    ErrorReason[ErrorReason["permissionDenied"] = 3] = "permissionDenied";
+    ErrorReason[ErrorReason["readingError"] = 4] = "readingError";
+    ErrorReason[ErrorReason["noLnurlFound"] = 5] = "noLnurlFound";
+})(ErrorReason = exports.ErrorReason || (exports.ErrorReason = {}));
 class NFCReader {
     constructor() {
-        this.listening = false;
         // Checks if Web NFC is present
         if ('NDEFReader' in window) {
             this.available = true;
@@ -23,57 +31,133 @@ class NFCReader {
             this.available = false;
         }
     }
-    listen(callback) {
+    listen(signal) {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.available && this.ndefReader && !this.listening) {
-                yield this.ndefReader.scan();
-                this.ndefReader.addEventListener('readingerror', () => {
-                    throw new Error('Error reading nfc');
-                });
-                // @ts-ignore
-                this.ndefReader.addEventListener('reading', ({ message }) => {
-                    const record = message.records[0];
-                    const textDecoder = new TextDecoder('utf-8');
-                    // Decode NDEF data from tag, and remove lightning: prefix
-                    const lnurl = textDecoder.decode(record.data);
-                    // return the decoded lnurl
-                    callback(lnurl);
-                });
-                this.listening = true;
+            if (!this.available || !this.ndefReader) {
+                return Promise.reject(ErrorReason.unavailable);
             }
+            try {
+                yield this.ndefReader.scan(signal === undefined ? undefined : { signal });
+            }
+            catch (e) {
+                if (e instanceof DOMException && !!e.name) {
+                    switch (e.name) {
+                        case 'AbortError':
+                            return Promise.reject(ErrorReason.aborted);
+                        case 'InvalidStateError':
+                            return Promise.reject(ErrorReason.scanInProgress);
+                        case 'NotAllowedError':
+                            return Promise.reject(ErrorReason.permissionDenied);
+                        case 'NotSupportedError':
+                            return Promise.reject(ErrorReason.unavailable);
+                    }
+                }
+                throw e;
+            }
+            return new Promise((resolve, reject) => {
+                if (!this.ndefReader) {
+                    return reject(ErrorReason.unavailable);
+                }
+                if (signal !== undefined) {
+                    signal.onabort = () => reject(ErrorReason.aborted);
+                }
+                this.ndefReader.onreadingerror = () => this._onReadingError(reject);
+                this.ndefReader.onreading = (event) => this._onReading(event, resolve, reject);
+            });
         });
+    }
+    _onReadingError(reject) {
+        return reject(ErrorReason.readingError);
+    }
+    _onReading(event, resolve, reject) {
+        if (!event ||
+            !event.message ||
+            !event.message.records ||
+            !Array.isArray(event.message.records)) {
+            return reject(ErrorReason.readingError);
+        }
+        const decoder = new TextDecoder('utf-8');
+        const alternatives = [];
+        for (let record of event.message.records) {
+            if (!record || !record.data) {
+                continue;
+            }
+            try {
+                const currentValue = decoder.decode(record.data);
+                const lowercase = currentValue.toLowerCase();
+                if (lowercase.startsWith('lightning:')) {
+                    // Is an oldschool bech32 encoded lnurlw, remove lightning: and bech32 decode the rest.
+                    const lnurl = currentValue.slice('lightning:'.length);
+                    const decoded = bech32_1.bech32.decode(lnurl, 2000);
+                    const bytes = bech32_1.bech32.fromWords(decoded.words);
+                    const result = decoder.decode(new Uint8Array(bytes));
+                    return resolve(result);
+                }
+                else if (lowercase.startsWith('lnurlw://')) {
+                    // Is a new age lnurlw. Replace lnurlw with https, or http if it's an onion.
+                    let replaceWith = 'https://';
+                    if (_isOnion(currentValue)) {
+                        replaceWith = 'http://';
+                    }
+                    return resolve(replaceWith + currentValue.slice(9));
+                }
+                else if (lowercase.startsWith('lnurl') && lowercase.indexOf(':') === -1) {
+                    // Might be lnurl without 'lightning:' prefix. If decoding with bech32 works, use that.
+                    const decoded = bech32_1.bech32.decode(currentValue, 2000);
+                    const bytes = bech32_1.bech32.fromWords(decoded.words);
+                    const result = decoder.decode(new Uint8Array(bytes));
+                    return resolve(result);
+                }
+                else if (lowercase.startsWith('https://')) {
+                    // https:// might be the real deal. Add as alternative.
+                    alternatives.push(currentValue);
+                }
+                else if (lowercase.startsWith('http://')) {
+                    // http:// might be the real deal if it's an onion. Add as alternative.
+                    if (_isOnion(currentValue)) {
+                        alternatives.push(currentValue);
+                    }
+                }
+            }
+            catch (_a) {
+                /* Squelch. Decoding error. Record is garbage. */
+            }
+        }
+        // Apparently there was no good lnurl match in the ndef records array.
+        // Maybe there was a less obvious match. If so, return that.
+        if (alternatives.length > 0) {
+            return resolve(alternatives[0]);
+        }
+        return reject(ErrorReason.noLnurlFound);
     }
 }
 exports.NFCReader = NFCReader;
-function lnurlDecode(lnurl) {
-    if (lnurl.indexOf('lnurlw:') !== -1) {
-        // boltcard support
-        return lnurl.replace('lnurlw:', 'https:');
-    }
-    const result = bech32_1.bech32.decode(lnurl.replace('lightning:', ''), 2000);
-    const requestByteArray = bech32_1.bech32.fromWords(result.words);
-    return bin2String(requestByteArray);
+function _isOnion(potentialOnion) {
+    const lowercase = potentialOnion.toLowerCase();
+    return (lowercase &&
+        (lowercase.endsWith('.onion') ||
+            lowercase.indexOf('.onion/') >= 0 ||
+            lowercase.indexOf('.onion?') >= 0));
 }
-exports.lnurlDecode = lnurlDecode;
-function bin2String(array) {
-    let result = '';
-    for (let i = 0; i < array.length; i++) {
-        result += String.fromCharCode(array[i]);
-    }
-    return result;
-}
-exports.bin2String = bin2String;
 function handleLNURL(lnurl, invoice, proxy) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const decoded = lnurlDecode(lnurl);
-            const { callback, k1, reason, status } = yield (yield fetch(`${proxy}?url=${decoded}`)).json();
-            return status === 'ERROR'
-                ? {
+            const encodedLnurl = encodeURIComponent(lnurl);
+            const response = yield fetch(`${proxy}?url=${encodedLnurl}`);
+            const { callback, k1, reason, status } = yield response.json();
+            if (status === 'ERROR') {
+                return {
                     success: false,
                     message: reason,
-                }
-                : handlePayment(callback, k1, invoice, proxy);
+                };
+            }
+            if (!callback || typeof callback !== 'string' || !k1 || typeof k1 !== 'string') {
+                return {
+                    success: false,
+                    message: 'invalid response',
+                };
+            }
+            return handlePayment(callback, k1, invoice, proxy);
         }
         catch (e) {
             return {
@@ -87,17 +171,25 @@ exports.handleLNURL = handleLNURL;
 function handlePayment(callback, k1, invoice, proxy) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const paymentRequest = `${callback}?k1=${k1}&pr=${invoice.replace('lightning:', '')}`;
+            const separator = callback.indexOf('?') === -1 ? '?' : '&';
+            const paymentRequest = `${callback}${separator}k1=${k1}&pr=${invoice.replace('lightning:', '')}`;
             const paymentResult = yield (yield fetch(`${proxy}?url=${encodeURIComponent(paymentRequest)}`)).json();
-            return paymentResult.status === 'ERROR'
-                ? {
+            if (paymentResult.status === 'OK') {
+                return {
+                    success: true,
+                    message: 'invoice payment initiated',
+                };
+            }
+            if (paymentResult.status === 'ERROR') {
+                return {
                     success: false,
                     message: paymentResult.reason,
-                }
-                : {
-                    success: true,
-                    message: 'invoice paid!',
                 };
+            }
+            return {
+                success: false,
+                message: 'Invalid reponse',
+            };
         }
         catch (e) {
             return {
